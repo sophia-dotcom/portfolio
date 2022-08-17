fe721547f8d07dcefc911fccdfb3fb2df374e8f7
sophia123 <sophiachew333@gmail.com>
2022-08-17 16:07:36 +0800

    make all boxes show up on mousedown

--- script.js
+++ script.js
@@ -9,25 +9,25 @@ bg.addEventListener("mouseover", function (e) {
   // console.log(e.target);
 
   boxdiv.innerHTML =
-    "<div class='newdiv'><div id='box-1' style='border-radius:3px; width: 75px; height: 75px; position: relative; top: -270px; left: 220px; z-index: 2'></div><div id='box-2' style='border-radius:3px; width: 75px;height: 75px; position: relative;top: -320px;left: 40px;z-index: 3;'> </div><div id='box-3' style='border-radius:3px; width: 75px;height: 75px;position: relative;top: -290px;left: 140px;z-index: 3;'></div></div>";
+    "<div class='newdiv'><div id='box-1' style='background-color: rgba(222, 217, 82, 0.8); border-radius:3px; width: 75px; height: 75px; position: relative; top: -270px; left: 220px; z-index: 2'><h4 id='box-1-text' style='padding: 4px' >Tik Tok</h4></div><div id='box-2' style='background-color: rgba(222, 217, 82, 0.8); border-radius:3px; width: 75px;height: 75px; position: relative;top: -320px;left: 40px;z-index: 3;'><h4 id='box-2-text' style='padding: 3px' >Graphic Design</h4></div><div id='box-3' style='background-color: rgba(222, 217, 82, 0.8); border-radius:3px; width: 75px;height: 75px;position: relative;top: -290px;left: 140px;z-index: 3;'><h4 id='box-3-text' style='padding: 5px; padding-top: 50px;color:rgb(0,0,0);'>More</h4></div></div>";
 
-  const box1 = document.querySelector("#box-1");
-  const box2 = document.querySelector("#box-2");
-  const box3 = document.querySelector("#box-3");
+  // const box1 = document.querySelector("#box-1");
+  // const box2 = document.querySelector("#box-2");
+  // const box3 = document.querySelector("#box-3");
 
   boxdiv.addEventListener("mouseover", function (e) {
-    if (e.target.id === "box-1") {
-      box1.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
-      box1.innerHTML = "<h4 id='box-1-text' style='padding: 4px' >Tik Tok</h4>";
-    } else if (e.target.id === "box-2") {
-      box2.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
-      box2.innerHTML =
-        "<h4 id='box-2-text' style='padding: 3px' >Graphic Design</h4>";
-    } else if (e.target.id === "box-3") {
-      box3.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
-      box3.innerHTML =
-        "<h4 id='box-3-text' style='padding: 5px; padding-top: 50px;color:rgb(0,0,0);'>More</h4>";
-    }
+    //   if (e.target.id === "box-1") {
+    //     box1.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
+    //     box1.innerHTML = "<h4 id='box-1-text' style='padding: 4px' >Tik Tok</h4>";
+    //   } else if (e.target.id === "box-2") {
+    //     box2.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
+    //     box2.innerHTML =
+    //       "<h4 id='box-2-text' style='padding: 3px' >Graphic Design</h4>";
+    //   } else if (e.target.id === "box-3") {
+    //     box3.style.backgroundColor = "rgba(222, 217, 82, 0.8)";
+    //     box3.innerHTML =
+    //       "<h4 id='box-3-text' style='padding: 5px; padding-top: 50px;color:rgb(0,0,0);'>More</h4>";
+    //   }
     boxdiv.addEventListener("click", function (e) {
       // console.log(e.target);
       if (e.target.id === "box-1" || e.target.id === "box-1-text") {
