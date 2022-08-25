92610c1d280015c641dbaba7d51daf5de27004a8
sophia123 <sophiachew333@gmail.com>
2022-08-25 23:47:49 +0800

    edit more stuff

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ bg.addEventListener("mouseover", function (e) {
       // console.log(e.target);
       if (e.target.id === "box-1" || e.target.id === "box-1-text") {
         aboutme.innerHTML =
-          "<h4>Tik Tok</h4><p>I researched crypto and NFT content on YouTube, tiktok and Twitter and published TikToks based on my research. The Tik Toks performed quite well and in 3 months, Tiktok follower growth hit 1,000 followers, with TikToks having an average 3,000-5,000 views.<br/><br/> <a href='https://www.tiktok.com/@coinstore_global/video/7034423185898573057' target='_blank' style='margin-left: 40%;'>Example:<br/><br/><img src='tiktokeg.png' width='150px' style='margin-left: 32.5%;'></a> </p>";
+          "<h4>Tik Tok</h4><p>I researched crypto and NFT content on YouTube, tiktok and Twitter and published TikToks based on my research. The Tik Toks performed quite well and in 3 months, Tiktok follower growth hit 1,000 followers, with TikToks having an average 3,000-5,000 views.<br/><br/> Example:<br/><br/><img src='tiktokeg.png' width='150px' style='margin-left: 32.5%;'></p>";
       } else if (e.target.id === "box-2" || e.target.id === "box-2-text") {
         aboutme.innerHTML =
           "<h4>Graphic Design</h4><p>These are some graphics that I created for a company’s Twitter, Medium and Facebook. They are meant for informing followers of news. The aesthetic and needs to be in line with the company’s branding. </p><img width='300px' src='idm.png'> <img width='300px' src='microverse.png'> <p>I designed some graphics to be posted on Facebook, LinkedIn and Instagram for a company. This is to portray the brand’s image and give the company a personality that people can relate to.</p> <img width='300px' src='texting.png'> <img width='300px' src='macbook.png'>";
